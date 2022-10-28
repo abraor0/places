0288b6009f36dc4bb6aabafdd680dce7fc778870
Abrão Rodrigues <arnf2710@gmail.com>
2022-10-28 19:13:10 -0300

    Defines the ui for the form and inputs

---
+++ src/components/Form.js
@@ -0,0 +1,88 @@
+const Form = () => {
+  return (
+    <form action="#" method="POST">
+      <div className="overflow-hidden shadow sm:rounded-md">
+        <div className="bg-white px-4 py-5 sm:p-6">
+          <div className="grid grid-cols-6 gap-6">
+            <div className="col-span-6 sm:col-span-3">
+              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                Nome
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3">
+              <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="text"
+                id="email-address"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3">
+              <label htmlFor="telephone" className="block text-sm font-medium text-gray-700">
+                Telefone
+              </label>
+              <input
+                type="text"
+                id="telephone"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3">
+              <label htmlFor="cpf" className="block text-sm font-medium text-gray-700">
+                CPF
+              </label>
+              <input
+                type="text"
+                id="cpf"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3">
+              <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                País
+              </label>
+              <select
+                id="country"
+                className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              >
+              </select>
+            </div>
+
+            <div className="col-span-6 sm:col-span-3">
+              <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                Cidade
+              </label>
+              <select
+                id="city"
+                className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              >
+              </select>
+            </div>
+
+          </div>
+        </div>
+        <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button
+            type="submit"
+            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Enviar
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
