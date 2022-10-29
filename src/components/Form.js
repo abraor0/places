33efde300d4afe9ee4dae1a423530c3a87faf880
Abrão Rodrigues <arnf2710@gmail.com>
2022-10-29 11:26:36 -0300

    Sets up initial version of the form logic

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,27 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { getAvailableCountriesAndCities } from '../apis';
+
 const Form = () => {
+  const { register, handleSubmit, watch } = useForm();
+  const [countriesList, setCountriesList] = useState();
+  const selectedCountry = watch('pais');
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const countries = await getAvailableCountriesAndCities();
+      setCountriesList(countries);
+    }
+    fetchCountries();
+  }, []);
+
+  let filteredCities = [], mappedCities = [];
+  if (countriesList && selectedCountry) {
+    filteredCities = countriesList.find(country => country.code === selectedCountry).cities?.sort((a, b) => a.name.localeCompare(b.name));
+    mappedCities = filteredCities.map(({ country_code, name, id }) => <option key={id} value={country_code}>{name}</option>);
+  }
+
   return (
     <form action="#" method="POST">
       <div className="overflow-hidden shadow sm:rounded-md">
@@ -12,6 +35,7 @@ const Form = () => {
                 type="text"
                 id="name"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                {...register('nome')}
               />
             </div>
 
@@ -23,6 +47,7 @@ const Form = () => {
                 type="text"
                 id="email-address"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                {...register('email')}
               />
             </div>
 
@@ -34,6 +59,7 @@ const Form = () => {
                 type="text"
                 id="telephone"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                {...register('telefone')}
               />
             </div>
 
@@ -45,6 +71,7 @@ const Form = () => {
                 type="text"
                 id="cpf"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                {...register('cpf')}
               />
             </div>
 
@@ -53,9 +80,12 @@ const Form = () => {
                 País
               </label>
               <select
+                disabled={!countriesList}
                 id="country"
                 className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                {...register('pais')}
               >
+                {countriesList && countriesList.map(({ code, name, index }) => <option key={code} selected={index === 0} value={code}>{name}</option>)}
               </select>
             </div>
 
@@ -64,9 +94,12 @@ const Form = () => {
                 Cidade
               </label>
               <select
+                disabled={!selectedCountry || filteredCities.length === 0}
                 id="city"
                 className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                {...register('cidade')}
               >
+                {mappedCities}
               </select>
             </div>
 
